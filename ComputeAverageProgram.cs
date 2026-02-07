9da7f2b01d97773858ab43e2c7d57b53f8db3393
beetsoul <159931577+beetsoul@users.noreply.github.com>
2026-02-07 03:54:30 -0800

    Add files via upload

---
+++ ComputeAverageProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int count = 5;
+
+ 
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            for (int i = 0; i < count; i++)
+            {
+
+                if (int.TryParse(Console.ReadLine(), out int grade))
+                {
+                    sum += grade; 
+                }
+                else
+                {
+    
+                    Console.WriteLine("Invalid input, please enter a number.");
+                    i--;
+                }
+            }
+
+            double average = (double)sum / count;
+
+            int rAverage = (int)Math.Round(average);
+
+            Console.WriteLine($"The average is {average} and round off to {rAverage}");
+
+            Console.WriteLine("Press any key to exit..........");
+
+            Console.ReadKey(true);
+        }
+    }
+}
